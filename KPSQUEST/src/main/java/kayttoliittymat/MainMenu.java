ae45301b4651c98557b729b86650bb2188d3fc49
Mikkel Leskelä <mikkel.leskela@mail.com>
2021-04-13 13:33:24 +0300

    Päävalikon ja maailman käyttöliittymien luokat toistaiseksi tyhjinä

--- KPSQUEST/src/main/java/kayttoliittymat/MainMenu.java
+++ KPSQUEST/src/main/java/kayttoliittymat/MainMenu.java
@@ -1,5 +1,25 @@
 package kayttoliittymat;
 
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+
 public class MainMenu {
+    private OverworldUI u = new OverworldUI();
+
+    public Scene testScene() {
+        BorderPane pane = new BorderPane();
+        Button b = new Button("Heipä hei");
+        pane.setCenter(b);
+        
+        pane.setMinSize(200, 200);
+        
+        
+        Scene scene = new Scene(pane);
+        b.setOnAction(k ->{
+           
+        });
+        return scene;
+    }
     
 }
---
+++ KPSQUEST/src/main/java/kayttoliittymat/OverworldUI.java
@@ -0,0 +1,15 @@
+package kayttoliittymat;
+
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+public class OverworldUI {
+
+    public Scene getTestOW() {
+        BorderPane pane = new BorderPane();
+        //Label l = new Label
+        //pane.setMinSize(400, 300);
+        return null;
+    }
+    
+}
