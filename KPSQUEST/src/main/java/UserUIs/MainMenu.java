19e392535def265075e6340f44678b1f9b4ad83f
Mikkel Leskelä <mikkel.leskela@mail.com>
2021-04-13 20:33:47 +0300

    Tyhjälle tallennukselle voi määrittää oman nimen, ei vielä tallennu

--- KPSQUEST/src/main/java/UserUIs/MainMenu.java
+++ KPSQUEST/src/main/java/UserUIs/MainMenu.java
@@ -1,26 +1,33 @@
 package UserUIs;
 
+import Utils.Savefile;
 import java.io.File;
 import java.util.Scanner;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class MainMenu {
-    private String[] files = new String[]{"File 1", "File 2", "File 3"};
+    private Savefile[] files = new Savefile[] {new Savefile("New File 1", false), new Savefile("New File 2", false), new Savefile("New File 3", false)};
     
     public void setMainMenuScene(Stage s) {
         BorderPane pane = new BorderPane();
         VBox buttons = new VBox();
         pane.setMinSize(200, 200);
         
-        for (String filename: files) {
-            Button b = new Button(filename);
+        for (Savefile file: files) {
+            Button b = new Button(file.name);
             
             b.setOnAction(p ->{
-                OverworldUI.setOverWorldScene(s, filename);
+                if (file.started) {
+                    OverworldUI.setOverWorldScene(s, file.name);
+                } else {
+                    this.createNewSave(s, file);
+                }
             });
             
             buttons.getChildren().add(b);
@@ -39,13 +46,36 @@ public class MainMenu {
                 if (s.length() == 0) {
                     //Tyhjä rivi
                 } else {
-                    files[i] = s;
+                    files[i] = new Savefile(s, true);
                 }
                 i++;
             }
         } catch (Exception e) {
             
         }
+    }
+
+    private void createNewSave(Stage s, Savefile file) {
+        BorderPane pane = new BorderPane();
+        Label text = new Label("Please insert your name");
+        TextField field = new TextField();
+        Button b = new Button("This is my name");
+        
+        
+        b.setOnAction(p ->{
+            if (field.getText().length() > 0) {
+                file.name = field.getText();
+                file.started = true;
+                this.setMainMenuScene(s);
+            }
+        });
+        
+        pane.setMinSize(200, 200);
+        pane.setTop(text);
+        pane.setCenter(field);
+        pane.setBottom(b);
+        Scene n = new Scene(pane);
+        s.setScene(n);
     }
     
 }
