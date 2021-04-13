e93118634a676a6aef19e2ff854ac49d07441810
Mikkel Leskelä <mikkel.leskela@mail.com>
2021-04-13 20:33:09 +0300

    Lisättiin apupakkaus Utils tallennuksia varten

---
+++ KPSQUEST/src/main/java/Utils/Savefile.java
@@ -0,0 +1,19 @@
+package Utils;
+
+public class Savefile {
+    public String name;
+    public boolean started;
+    
+    public Savefile(String name, boolean started) {
+        this.name = name;
+        this.started = started;
+    }
+    
+    public void changeName(String name) {
+        this.name = name;
+    }
+    
+    public void start() {
+        this.started = true;
+    }
+}
