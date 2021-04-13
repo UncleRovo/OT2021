9d8a5bdc2f6c77cf135338e05f5ebcf11dfc03c9
Mikkel Leskelä <mikkel.leskela@mail.com>
2021-04-13 13:32:37 +0300

    raakaversio käyttöliittymästä

--- KPSQUEST/src/main/java/kpsquest/KPSQuest.java
+++ KPSQUEST/src/main/java/kpsquest/KPSQuest.java
@@ -1,9 +1,47 @@
 package kpsquest;
 
-public class KPSQuest {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
+public class KPSQuest extends Application {
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        launch(KPSQuest.class);
+    }
+
+    @Override
+    public void start(Stage arg0) throws Exception {
+        setMainMenuScene(arg0);
+        arg0.show();
+    }
+    
+    public void setMainMenuScene(Stage s) {
+        BorderPane pane = new BorderPane();
+        Button b = new Button("Heipä hei");
+        pane.setCenter(b);
+        pane.setMinSize(200, 200);
+        
+        Scene mainmenu = new Scene(pane);
+        b.setOnAction(p ->{
+            setOverWorldScene(s);
+        });
+        
+        s.setScene(mainmenu);
+    }
+    
+    public void setOverWorldScene(Stage s) {
+        BorderPane pane2 = new BorderPane();
+        pane2.setMinSize(500, 300);
+        pane2.setCenter(new Label("Hurraa!"));
+        
+        Scene ovscene = new Scene(pane2);
+        s.setScene(ovscene);
     }
     
 }
+
+
