02dc23518b5f4fc856d70c20fc4c65742b1862dc
Mikkel Leskelä <mikkel.leskela@mail.com>
2021-04-17 21:15:31 +0300

    update

--- KPSQUEST/src/main/java/UserUIs/OverworldUI.java
+++ KPSQUEST/src/main/java/UserUIs/OverworldUI.java
@@ -28,13 +28,18 @@ public class OverworldUI {
         //background.setScaleX(0.5);
         background.setTranslateX(-900);
         
-        Image tr = new Image("file:kuusi_bluscreen.png");
-        ImageView tree = new ImageView(getImageWithoutBlue(tr));
-        tree.setScaleX(0.5);
-        tree.setScaleY(0.5);
+        Image pl = new Image("file:protagonist.jpg");
+        ImageView player = new ImageView(getImageWithoutBlue(pl));
+        
+        Image h_b = new Image("file:talo_ala.jpg");
+        ImageView house1_bottom = new ImageView(getImageWithoutBlue(h_b));
+        
+        shrinkByAmount(player, 0.5);
+        shrinkByAmount(house1_bottom, 0.5);
         
         pane2.getChildren().add(background);
-        pane2.getChildren().add(tree);
+        pane2.getChildren().add(player);
+        pane2.getChildren().add(house1_bottom);
         pane2.getChildren().get(1).toFront();
         
         
@@ -45,19 +50,19 @@ public class OverworldUI {
             @Override
             public void handle(KeyEvent event) {
                 if (event.getCode() == KeyCode.LEFT) {
-                        tree.setTranslateX(tree.getTranslateX() - 5);
+                        player.setTranslateX(player.getTranslateX() - 5);
                     }
                 
                 if (event.getCode() == KeyCode.RIGHT) {
-                    tree.setTranslateX(tree.getTranslateX() + 5);
+                    player.setTranslateX(player.getTranslateX() + 5);
                 }
                 
                 if (event.getCode() == KeyCode.DOWN) {
-                        tree.setTranslateY(tree.getTranslateY() + 5);
+                        player.setTranslateY(player.getTranslateY() + 5);
                     }
                 
                 if (event.getCode() == KeyCode.UP) {
-                    tree.setTranslateY(tree.getTranslateY() - 5);
+                    player.setTranslateY(player.getTranslateY() - 5);
                 }
             }
         });
@@ -69,6 +74,8 @@ public class OverworldUI {
         PixelReader reader = image.getPixelReader();
         PixelWriter writer = wi.getPixelWriter();
         
+        System.out.println(Color.BLUE.toString() + "   " + reader.getColor(0, 0).toString());
+        
         for (int y = 0; y < image.getHeight(); y++) {
             for (int x = 0; x < image.getWidth(); x++) {
                 if (!(reader.getColor(x, y).toString().equals(Color.BLUE.toString()))) {
@@ -78,4 +85,9 @@ public class OverworldUI {
         }
         return wi;
     }
+
+    private static void shrinkByAmount(ImageView image, double d) {
+        image.setScaleX(d);
+        image.setScaleY(d);
+   }
 }
