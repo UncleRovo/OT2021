92b1b78e27f5a682dbed27fb2a960456bde247da
Mikkel Leskelä <mikkel.leskela@mail.com>
2021-04-20 21:31:15 +0300

    lisättiin toiminnallisuus tiedostojen lukemiseen

---
+++ KPSQUEST/src/main/java/Utils/Utils.java
@@ -0,0 +1,20 @@
+package Utils;
+public class Utils {
+    public static String getFilePath(String path) {
+        
+        String[] name = path.split(":");
+        
+        ClassLoader cl = Thread.currentThread().getContextClassLoader();
+        if (cl == null) {
+            cl = Class.class.getClassLoader();
+        }
+        cl.getResourceAsStream(name[1]);
+        
+        //System.out.println(cl.getResource(name[1]));
+        
+        if (cl.getResource(name[1]) != null) {
+            path = cl.getResource(name[1]).toString();
+        }
+        return path;
+    } 
+}
