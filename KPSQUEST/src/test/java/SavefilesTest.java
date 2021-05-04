6aade60ec5ea79aa239827ee8df61311024cbb3c
Mikkel Leskelä <mikkel.leskela@mail.com>
2021-05-04 22:10:31 +0300

    Savefiles -luokan testejä

---
+++ KPSQUEST/src/test/java/SavefilesTest.java
@@ -0,0 +1,34 @@
+import data.Savefiles;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author mikkel
+ */
+public class SavefilesTest {
+    Savefiles s;
+    
+    @Before
+    public void setUp() {
+        s = new Savefiles();
+        s.initFiles();
+    }
+    
+    @Test
+    public void deletionWorks() {
+        s.deleteData();
+        assertEquals(false, s.files[0].started);
+        assertEquals(false, s.files[1].started);
+        assertEquals(false, s.files[2].started);
+    }
+
+    @Test
+    public void startSaveWorks() {
+        s.deleteData();
+        s.files[0].name = "jyrmy";
+        s.createNew(s.files[0], 1);
+        assertEquals("jyrmy", s.files[0].name);
+    }
+}
