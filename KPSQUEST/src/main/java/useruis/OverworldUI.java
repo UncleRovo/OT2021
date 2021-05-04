ef4c2b8b3fe08e4ed9b9b1dacc0680e74e77d8bb
Mikkel Leskelä <mikkel.leskela@mail.com>
2021-05-04 20:58:59 +0300

    refaktorointia

--- KPSQUEST/src/main/java/useruis/OverworldUI.java
+++ KPSQUEST/src/main/java/useruis/OverworldUI.java
@@ -1,30 +1,31 @@
 package useruis;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import javafx.event.EventHandler;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
 import javafx.scene.shape.Polygon;
 import javafx.stage.Stage;
 
 public class OverworldUI {
     
     public static void setOverWorldScene(Stage s, String name) {
+        List<Node> elements = new ArrayList<>();
         HashSet<Polygon> hitboxes = new HashSet<>();
         
         Pane pane2 = new Pane();
         pane2.setMinSize(500, 300);
         pane2.setMaxSize(700, 400);
         
-        ImageView background = GraphicsBuilder.getGraphicsObject("file:tausta_test.jpg", -900, 0, 1, false);
-        ImageView player = GraphicsBuilder.getGraphicsObject("file:protagonist.png", 0, 0, 0.5, true);
-        ImageView house1Bottom = GraphicsBuilder.getGraphicsObject("file:talo_ala.png", 250, 700, 0.5, true);
-        ImageView house1Top = GraphicsBuilder.getGraphicsObject("file:talo_yla.png", 241, 578, 0.5, true);
-        ImageView rival = GraphicsBuilder.getGraphicsObject("file:rival.png", 1250, 0, 0.5, true);
+        buildGraphics(elements);
+        
+        
         
         Polygon houseHitbox = GraphicsBuilder.getPolygon(0, 0, 255, 0, 255, 20, 0, 20, false, 390, 734);
         Polygon rivalHitbox = GraphicsBuilder.getPolygon(0, 0, 40, 0, 40, 140, 0, 140, false, 1320, 70);
@@ -35,7 +36,7 @@ public class OverworldUI {
         hitboxes.add(houseHitbox);
         hitboxes.add(rivalHitbox);
         
-        pane2.getChildren().addAll(background, house1Bottom, rival, player, house1Top/*, dialogueBox*/);
+        pane2.getChildren().addAll(elements);
         pane2.getChildren().addAll(houseHitbox, rivalHitbox, rivalTalkbox);
         
         DialogueHandler convo = new DialogueHandler();
@@ -45,6 +46,7 @@ public class OverworldUI {
         
         ovscene.setOnKeyPressed(new EventHandler<KeyEvent>() {
             DialogueHandler diaHandler = convo;
+            Node player = elements.get(3);
             KeyCode latest = null;
             @Override
             public void handle(KeyEvent event) {
@@ -92,14 +94,27 @@ public class OverworldUI {
             }
         });
         s.setScene(ovscene);
-        //s.setMaximized(true);
-        s.setHeight(background.getImage().getHeight());
-        s.setWidth(background.getImage().getWidth() * 0.7);
-        s.setMaxHeight(background.getImage().getHeight());
-        s.setMaxWidth(background.getImage().getWidth() * 0.7);
+        s.setMaximized(false);
+        s.setHeight(1200);
+        s.setWidth(2000);
+        s.setMaxHeight(1500);
+        s.setMaxWidth(2400);
     }
     
-    public static boolean collision(HashSet<Polygon> hitboxes, ImageView object) {
+    public static boolean collision(HashSet<Polygon> hitboxes, Node object) {
         return hitboxes.stream().anyMatch((hitbox) -> (object.getBoundsInParent().intersects(hitbox.getBoundsInParent())));
+    }
+
+    private static void buildGraphics(List<Node> elements) {
+        ImageView background = GraphicsBuilder.getGraphicsObject("file:tausta_test.jpg", -900, 0, 1, false);
+        ImageView player = GraphicsBuilder.getGraphicsObject("file:protagonist.png", 0, 0, 0.5, true);
+        ImageView house1Bottom = GraphicsBuilder.getGraphicsObject("file:talo_ala.png", 250, 700, 0.5, true);
+        ImageView house1Top = GraphicsBuilder.getGraphicsObject("file:talo_yla.png", 241, 578, 0.5, true);
+        ImageView rival = GraphicsBuilder.getGraphicsObject("file:rival.png", 1250, 0, 0.5, true);
+        elements.add(background);
+        elements.add(house1Bottom);
+        elements.add(rival);
+        elements.add(player);
+        elements.add(house1Top);
     }
 }
