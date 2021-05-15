6688316eaeeae5deeddcf6b05bb0e6977537739f
Mikkel Leskelä <mikkel.leskela@mail.com>
2021-05-15 23:48:41 +0300

    keskeneräinen versio taisteluruudusta

--- KPSQUEST/src/main/java/useruis/Battlehandler.java
+++ KPSQUEST/src/main/java/useruis/Battlehandler.java
@@ -15,29 +15,41 @@ public class Battlehandler {
     String enemy;
     List<Node> elements;
     Pane pane;
+    Label p;
+    Label e;
+    Button b;
+    Polygon poly;
     
     public Battlehandler(List<Node> elements, Pane pane) {
         player = "Player";
         enemy = "Rival";
         this.elements = elements;
-        Label p = new Label(player);
-        Label e = new Label(enemy);
-        Button b = new Button("PAINA TÄSTÄ");
+        p = new Label(player);
+        e = new Label(enemy);
+        b = new Button("PAINA TÄSTÄ");
         b.setOnMouseClicked(c ->{
             this.endBattle();
         });
-        view.add(p, 0, 0);
-        view.add(e, 2, 0);
-        view.add(b, 1, 1);
+        poly = GraphicsBuilder.getPolygon(700, 700, 390, 390, true, 0, 0);
+        poly.setFill(Color.RED);
+        move(poly, 500, 100);
+        move(p, 500, 100);
+        move(e, 900, 100);
+        move(b, 700, 300);
         this.pane = pane;
     }
     
     public void startBattle() {
         System.out.println("Taistelu alkoi");
-        pane.getChildren().addAll(view);
+        pane.getChildren().addAll(poly,p, e, b);
     }
     
     public void endBattle() {
-        pane.getChildren().removeAll(view);
+        pane.getChildren().removeAll(poly, p, e, b);
+    }
+    
+    public void move(Node n, double x, double y) {
+        n.setTranslateX(x);
+        n.setTranslateY(y);
     }
 }
